a3a942bc6f99a32d14072ec5b37c4517773cb3ff
Aza Raskin <aza@air.local>
2013-10-27 01:03:40 -0700

    zzz

--- love_dispatcher.js
+++ love_dispatcher.js
@@ -32,7 +32,7 @@ exports.callBomber = function(number){
       to:'+1' + number, // Any number Twilio can call
       from: '+16503535591', // A number you bought from Twilio and can use for outbound communication
       url: 'http://lovebomb.herokuapp.com/xml/record.xml',
-      Record: true,
+      Record: false,
       StatusCallback: 'http://lovebomb.herokuapp.com/recording',
       StatusCallbackMethod: 'GET'
   }, function(err, responseData) {
